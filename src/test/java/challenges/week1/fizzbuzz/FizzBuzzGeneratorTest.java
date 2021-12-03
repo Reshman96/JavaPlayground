6be7c389412685064ad853b09b951c01c39f47ab
resh <reshmansiva96@gmail.com>
2021-12-03 10:59:32 +0000

    removing repetitive code

--- src/test/java/challenges/week1/fizzbuzz/FizzBuzzGeneratorTest.java
+++ src/test/java/challenges/week1/fizzbuzz/FizzBuzzGeneratorTest.java
@@ -5,37 +5,34 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzzGeneratorTest {
 
+    FizzBuzzGenerator generator = new FizzBuzzGenerator();
+
     @Test
     public void thirdItemIsBuzzTest() {
-        FizzBuzzGenerator generator = new FizzBuzzGenerator();
 
         Assertions.assertEquals("Fizz", generator.FizzBuzz(20).get(2));
     }
 
     @Test
     public void fifthItemIsBuzzTest() {
-        FizzBuzzGenerator generator = new FizzBuzzGenerator();
 
         Assertions.assertEquals("Buzz", generator.FizzBuzz(20).get(4));
     }
 
     @Test
     public void fifteenthItemIsBuzzTest() {
-        FizzBuzzGenerator generator = new FizzBuzzGenerator();
 
         Assertions.assertEquals("FizzBuzz", generator.FizzBuzz(20).get(14));
     }
 
     @Test
     public void nIs100Test() {
-        FizzBuzzGenerator generator = new FizzBuzzGenerator();
 
         Assertions.assertEquals("Buzz", generator.FizzBuzz(100).get(99));
     }
 
     @Test
     public void twentySecondItemIsNotReplacedTest() {
-        FizzBuzzGenerator generator = new FizzBuzzGenerator();
 
         Assertions.assertEquals("22", generator.FizzBuzz(40).get(21));
     }
