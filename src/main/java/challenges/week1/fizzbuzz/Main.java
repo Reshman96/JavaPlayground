a45a3e78dc6cf9631c35c2f063a15d985a0e53aa
resh <reshmansiva96@gmail.com>
2021-12-03 10:20:19 +0000

    FizzBuzzGenerator created

---
+++ src/main/java/challenges/week1/fizzbuzz/Main.java
@@ -0,0 +1,10 @@
+package challenges.week1.fizzbuzz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FizzBuzzGenerator generator = new FizzBuzzGenerator();
+
+        System.out.println(generator.FizzBuzz(100));
+    }
+}
