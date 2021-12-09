f95cfcfc73bff24db54d904c8e25298e9804fb68
resh <reshmansiva96@gmail.com>
2021-12-09 18:56:16 +0000

    creating class

--- src/test/java/challenges/week2/test/SpartanTest.java
+++ src/test/java/challenges/week2/test/SpartanTest.java
@@ -1,37 +1,56 @@
 package challenges.week2.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class SpartanTest {
     private static Spartan spartan;
 
+
     @BeforeAll
-    static void setup() {
-        spartan = new Spartan(1234, "Resh", "Java Developer", LocalDate.parse("2021-11-29", DateTimeFormatter.ISO_DATE));
+    static void setupAll(TestInfo testInfo) {
+        System.out.println(testInfo.getTestClass() + " executing");
     }
 
-    @Test
-    void getIdTest() {
-        Assertions.assertEquals(1234, spartan.getId());
+    @BeforeEach
+    void setup() {
+        spartan = new Spartan(1, "Resh", "Java", LocalDate.of(2021, 1, 1));
     }
 
-    @Test
-    void getNameTest() {
-        Assertions.assertEquals("Resh", spartan.getName());
+    @RepeatedTest(value = 5, name = "displayName} :: repetition {currentRepetition} of {totalRepition}")
+    @DisplayName("This is my test")
+    void thisIsMyTest() {
+
     }
 
-    @Test
-    void getCourseTest() {
-        Assertions.assertEquals("Java Developer", spartan.getCourse());
+    @AfterEach
+    void tearDown(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName() + " has finished testing");
     }
 
-    @Test
-    void getStartDateTest() {
-        Assertions.assertEquals(LocalDate.parse("2021-11-29", DateTimeFormatter.ISO_DATE), spartan.getStartDate());
+    @AfterAll
+    static void tearDownAll(TestInfo testInfo) {
+        System.out.println(testInfo.getTestClass() + " completed");
     }
+
+//    @Test
+//    void getIdTest() {
+//        Assertions.assertEquals(1, spartan.getId());
+//    }
+//
+//    @Test
+//    void getNameTest() {
+//        Assertions.assertEquals("Resh", spartan.getName());
+//    }
+//
+//    @Test
+//    void getCourseTest() {
+//        Assertions.assertEquals("Java", spartan.getCourse());
+//    }
+//
+//    @Test
+//    void getStartDateTest() {
+//        Assertions.assertEquals(LocalDate.parse("2021-11-29", DateTimeFormatter.ISO_DATE), spartan.getStartDate());
+//    }
 }
