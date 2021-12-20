b5263a87386d47b13b6e52d833d0928c33e39ac2
resh <reshmansiva96@gmail.com>
2021-12-20 22:34:33 +0000

    removing unnecessary lines

--- src/main/java/challenges/week4/file_io/FileOfExamples.java
+++ src/main/java/challenges/week4/file_io/FileOfExamples.java
@@ -6,17 +6,13 @@ public class FileOfExamples {
 
     public static void printLines() {
         String fileLocation = "src/main/resources/twas_the_night_before_christmas.txt";
-
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileLocation));
-
             String line = bufferedReader.readLine();
             while (line != null) {
                 System.out.println(line);
                 line = bufferedReader.readLine();
-
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -25,7 +21,6 @@ public class FileOfExamples {
     public static void printAllLinesUsingForLoop(String fileLocation) {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileLocation));
-
             for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
                 System.out.println(line);
             }
@@ -33,7 +28,6 @@ public class FileOfExamples {
             e.printStackTrace();
             System.out.println("File not found!");
         }
-
     }
 
     public static void writeToAFile() {
@@ -41,7 +35,6 @@ public class FileOfExamples {
           BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/write_example.txt"));
           bufferedWriter.write("this is a new line");
           bufferedWriter.close();
-
       } catch (IOException e) {
           e.printStackTrace();
       }
