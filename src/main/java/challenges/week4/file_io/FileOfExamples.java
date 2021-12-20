c68d5478c3d869a05c54d82ddb4b8e03782a9a28
resh <reshmansiva96@gmail.com>
2021-12-20 10:08:19 +0000

    adding a for loop version

--- src/main/java/challenges/week4/file_io/FileOfExamples.java
+++ src/main/java/challenges/week4/file_io/FileOfExamples.java
@@ -1,5 +1,6 @@
 package challenges.week4.file_io;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -9,12 +10,13 @@ public class FileOfExamples {
         String fileLocation = "src/main/resources/twas_the_night_before_christmas.txt";
 
         try {
-            FileReader fileReader = new FileReader(fileLocation);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileLocation));
 
-            int character = fileReader.read();
-            while (character != -1) {
-                System.out.println(fileReader.read());
-                character = fileReader.read();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+
             }
 
         } catch (IOException e) {
@@ -22,7 +24,22 @@ public class FileOfExamples {
         }
     }
 
+    public static void printAllLinesUsingForLoop(String fileLocation) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileLocation));
+
+            for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("File not found!");
+        }
+
+    }
+
     public static void main(String[] args) {
         FileOfExamples.printLines();
+//        FileOfExamples.printAllLinesUsingForLoop("src/main/resources/twas_the_night_before_christmas.txt");
     }
 }
