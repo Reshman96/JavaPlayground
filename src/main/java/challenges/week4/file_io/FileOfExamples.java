39a705682a19e078a416216bf60499b91f9a5e8b
resh <reshmansiva96@gmail.com>
2021-12-20 09:53:13 +0000

    fileOfExamples now working

--- src/main/java/challenges/week4/file_io/FileOfExamples.java
+++ src/main/java/challenges/week4/file_io/FileOfExamples.java
@@ -1,8 +1,28 @@
 package challenges.week4.file_io;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileOfExamples {
 
-    public void printLines() {
+    public static void printLines() {
+        String fileLocation = "src/main/resources/twas_the_night_before_christmas.txt";
 
+        try {
+            FileReader fileReader = new FileReader(fileLocation);
+
+            int character = fileReader.read();
+            while (character != -1) {
+                System.out.println(fileReader.read());
+                character = fileReader.read();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        FileOfExamples.printLines();
     }
 }
